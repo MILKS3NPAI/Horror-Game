f889509ca470072ceec856f8f9b43e9a462ad7db
kevink02 <kevinkubota0@gmail.com>
2021-09-12 22:14:27 -0700

    Trying to fix things

---
+++ HorrorGame/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public void SoundMenu()
+    {
+        Debug.Log("Sound menu");
+    }
+    public void MenuReturn()
+    {
+        Debug.Log("Back in menu");
+    }
+    public void ExitGame()
+    {
+        Debug.Log("You quit");
+    }
+}
---
+++ HorrorGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    [SerializeField] GameObject pauseMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        isPaused = false;
+        Time.timeScale = 1;
+    }
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        isPaused = true;
+        Time.timeScale = 0;
+    }
+    public void SoundMenu()
+    {
+        Debug.Log("Sound menu");
+    }
+    public void ExitGame()
+    {
+        Debug.Log("You quit");
+    }
+}
