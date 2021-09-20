24f89a847514170857c2828f91794cb9abc02293
kevink02 <kevinkubota0@gmail.com>
2021-09-19 20:52:08 -0700

    Music now plays based on player position

--- HorrorGame/Assets/Scripts/AudioManager.cs
+++ HorrorGame/Assets/Scripts/AudioManager.cs
@@ -12,9 +12,8 @@ public class AudioManager : MonoBehaviour
     {
         foreach (Sound s in sounds)
         {
-            
-
             s.source = gameObject.AddComponent<AudioSource>();
+            s.source.loop = s.canLoop;
             s.source.clip = s.clip;
             s.source.volume = s.volume;
         }
@@ -26,10 +25,77 @@ public class AudioManager : MonoBehaviour
         PlaySound("Music2");
     }
 
+    private void Update()
+    {
+        if (PlayerRoom().Equals("right"))
+        {
+            //Debug.Log("Player is in the right room.");
+            MuteSound("Music2");
+            UnmuteSound("Music1");
+        }
+        else if (PlayerRoom().Equals("left"))
+        {
+            //Debug.Log("Player is in the left room.");
+            MuteSound("Music1");
+            UnmuteSound("Music2");
+        }
+        else
+        {
+            Debug.Log("Player is not in a room.");
+        }
+    }
+
     public void PlaySound(string name)
     {
-        // Find the sound with the input name
         Sound s = Array.Find(sounds, sound => sound.name == name);
-        s.source.Play();
+        if (s != null)
+        {
+            s.source.Play();
+        }
+    }
+    public void PauseSound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s != null)
+        {
+            s.source.Pause();
+        }
+    }
+    public void MuteSound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s != null)
+        {
+            s.source.volume = 0;
+        }
+    }
+    public void UnmuteSound(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s != null)
+        {
+            s.source.volume = s.volume;
+        }
+    }
+
+    // Determines what room the player is in
+    private string PlayerRoom()
+    {
+        GameObject player = FindObjectOfType<Player>().gameObject;
+        if (player != null)
+        {
+            if (player.transform.position.x >= 0)
+            {
+                return "right";
+            }
+            else
+            {
+                return "left";
+            }
+        }
+        else
+        {
+            return "";
+        }
     }
 }
--- HorrorGame/Assets/Scripts/Sound.cs
+++ HorrorGame/Assets/Scripts/Sound.cs
@@ -9,6 +9,7 @@ public class Sound
     public AudioClip clip;
     [HideInInspector]
     public AudioSource source;
+    public bool canLoop;
     [Range(0, 1)]
     public float volume;
     public string name;
