9e9002c531e50aa04165474e7ffdfa67ba5ff871
ADumbDoor <adumbdoor@gmail.com>
2021-10-10 14:52:10 -0700

    Door Trigger now inherits from Useable

--- HorrorGame/Assets/Scripts/DoorTrigger.cs
+++ HorrorGame/Assets/Scripts/DoorTrigger.cs
@@ -2,55 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorTrigger : MonoBehaviour
+public class DoorTrigger : Useable
 {
-    private PlayerControls playerControls;
-    private bool playerInRange;
-    public int id;
+	public int id;
 
-
-    private void Awake()
-    {
-        playerControls = new PlayerControls();
-    }
-
-    private void Start()
-    {
-        playerControls.UI.Interact.performed += _ => Use();
-    }
-
-    private void OnEnable()
-    {
-        playerControls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Disable();
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            playerInRange = true;
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            playerInRange = false;
-        }
-    }
-
-    private void Use()
-    {
-        if (playerInRange == true)
-        {
-            GameEvents.GM.DoorEnter(id);
-        }
-    }
+	public override void Use(Player iPlayer)
+	{
+		GameEvents.GM.DoorEnter(id);
+	}
 }
 
