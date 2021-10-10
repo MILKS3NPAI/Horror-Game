6fb7ae123bbdbe590b22c4d3906e76c723226352
Ron Riley Co <60789953+MILKS3NPAI@users.noreply.github.com>
2021-10-10 16:06:30 -0700

    -Prefabs Created -FlickerLight Script made

---
+++ HorrorGame/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class LightFlicker : MonoBehaviour
+{
+    [SerializeField] private float maxInterval;
+    private bool isFlick = false;
+    private float timeDelay;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFlick == false)
+        {
+            StartCoroutine(FlickerLight());
+        }
+    }
+
+    IEnumerator FlickerLight()
+    {
+        isFlick = true;
+        this.gameObject.GetComponent<Light2D>().enabled = false;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        this.gameObject.GetComponent<Light2D>().enabled = true;
+        timeDelay = Random.Range(0.01f, 0.2f);
+        yield return new WaitForSeconds(timeDelay);
+        isFlick = false;
+    }
+}
