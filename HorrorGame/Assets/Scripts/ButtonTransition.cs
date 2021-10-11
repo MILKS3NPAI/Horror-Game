018f58fc621d75745f82bdc8780fe93c1df96622
kevink02 <kevinkubota0@gmail.com>
2021-10-11 12:45:14 -0700

    Added button text transitions

---
+++ HorrorGame/Assets/Scripts/ButtonTransition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonTransition : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private TextMeshProUGUI buttonText;
+
+    void Start()
+    {
+        buttonText = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        buttonText.fontSize = 90;
+        buttonText.faceColor = new Color32(0, 0, 0, 255);
+        buttonText.outlineWidth = 0.2f;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        buttonText.fontSize = 75;
+        buttonText.faceColor = new Color32(255, 255, 255, 255);
+        buttonText.outlineWidth = 0;
+    }
+}
