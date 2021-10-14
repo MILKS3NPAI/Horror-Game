da2f10aab856e4c6cbe86d58b2a934a8adc83466
Ron Riley Co <60789953+MILKS3NPAI@users.noreply.github.com>
2021-10-14 16:58:23 -0700

    -Door function update to transform camera

--- HorrorGame/Assets/Scripts/DoorController.cs
+++ HorrorGame/Assets/Scripts/DoorController.cs
@@ -7,9 +7,11 @@ public class DoorController : MonoBehaviour
 {
     private PlayerControls playerControls;
     [SerializeField] private float dir; //1=up -1=down
+    [SerializeField] private Camera main;
     public int id;
     GameObject player;
     Vector3 pos;
+    Vector3 pos1;
 
     void Start()
     {
@@ -21,9 +23,16 @@ public class DoorController : MonoBehaviour
     {
         if (id == this.id)
         {
+            main.GetComponent<CameraFollow>().enabled = false;
             pos = player.GetComponent<Transform>().position;
             pos.y += 15 * dir;
             player.GetComponent<Transform>().position = pos;
+
+            
+            pos1 = main.GetComponent<Transform>().position;
+            pos1.y += 15 * dir;
+            main.GetComponent<Transform>().position = pos1;
+            main.GetComponent<CameraFollow>().enabled = true;
         }
     }
 }
